530085b60a9c3bb78529ad8d27c29fcaf837c8f0
fagner <tagnisp@hotmail.com>
2015-10-29 22:09:41 -0200

    Primeira Pagina

--- app/src/main/java/br/com/fagnerabsynth/aplicativo/Inicial.java
+++ app/src/main/java/br/com/fagnerabsynth/aplicativo/Inicial.java
@@ -1,16 +1,33 @@
 package br.com.fagnerabsynth.aplicativo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import br.com.fagnerabsynth.aplicativo.Views.Login;
+import br.com.fagnerabsynth.aplicativo.Views.Principal;
+
 public class Inicial extends AppCompatActivity {
+
+    //Coloca em um metodo statico o valor da sessao para consulta
+    private static boolean sessao = false;
+
+    public static boolean sessao() {
+        return sessao;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicial);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("sessao", 0);
+
+        inicia();
+
     }
 
     @Override
@@ -34,4 +51,29 @@ public class Inicial extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void inicia() {
+
+        //verifica se a sessao com o nome da pessoa esta ativa
+        String valor;
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("sessao", 0);
+        valor = pref.getString("sessao", null);
+
+        sessao = !TextUtils.isEmpty(valor);
+
+
+        //verifica se a sessao esta ativa ou nao e direciona para a pagina correta!
+        Intent intentado;
+
+        if (!sessao) {
+            intentado = new Intent(this, Login.class);
+        } else {
+            intentado = new Intent(this, Principal.class);
+        }
+
+        startActivity(intentado);
+
+    }
+
+
 }
