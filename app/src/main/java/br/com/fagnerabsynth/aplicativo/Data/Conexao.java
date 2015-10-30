9f5abad30f8150eee923f487504d5579d1065a1f
fagner <tagnisp@hotmail.com>
2015-10-30 15:44:59 -0200

    Modelo PRODUTOS e inclusão de base de dados para manipulação de produtos...

--- app/src/main/java/br/com/fagnerabsynth/aplicativo/Data/Conexao.java
+++ app/src/main/java/br/com/fagnerabsynth/aplicativo/Data/Conexao.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import br.com.fagnerabsynth.aplicativo.Models.ProdutosMOD;
+
 /**
  * Created by fagner on 29/10/15.
  */
@@ -15,17 +17,23 @@ import java.security.NoSuchAlgorithmException;
 
 public class Conexao extends SQLiteOpenHelper {
     public static final String TABELA = "usuarios";
+    public static final String TABELA2 = "produtos";
+
     private static final String ARQUIVO = "usuarios.db";
     private static final int VERSAO = 1;
     private static final String CRIARTABELA1 = "create table "
             + TABELA + "( id integer primary key autoincrement, email text not null, senha text not null);";
+    private static final String CRIARTABELA2 = "create table "
+            + TABELA2 + "( id integer primary key autoincrement, nome text not null, categoria text not null,descricao text not null, ativo flag INTEGER DEFAULT 0, valor DECIMAL(10,2) not null );";
+
+
     private SQLiteDatabase db;
 
     public Conexao(Context context) {
         super(context, ARQUIVO, null, VERSAO);
     }
 
-    public String md5(String s) {
+    private String md5(String s) {
 
         try {
 
@@ -50,6 +58,8 @@ public class Conexao extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL(CRIARTABELA1);
+        db.execSQL(CRIARTABELA2);
+
 
         Cursor rs = db.rawQuery("SELECT * FROM " + TABELA, null);
 
@@ -57,6 +67,20 @@ public class Conexao extends SQLiteOpenHelper {
             String[] dadosAdmin = {"admin@admin.com", md5("12345")};
             db.execSQL("Insert into " + TABELA + "(email,senha) values (?,?)", dadosAdmin);
         }
+    }
+
+
+    public boolean adicionaProduto(ProdutosMOD produto) {
+        try {
+            if (produto.id != 0) {
+                db.execSQL("Insert into " + TABELA2 + "(nome,categoria,descricao,ativo,valor) values (?,?,?,?,?)", new String[]{produto.nome, produto.categoria, produto.descricao, "" + produto.ativo, "" + produto.valor});
+            } else {
+                db.execSQL("update " + TABELA2 + " set nome=?,categoria=?,descricao=?,ativo=?,valor=? where id = ?", new String[]{produto.nome, produto.categoria, produto.descricao, "" + produto.ativo, "" + produto.valor, "" + produto.id});
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
 
---
+++ app/src/main/java/br/com/fagnerabsynth/aplicativo/Models/ProdutosMOD.java
@@ -0,0 +1,13 @@
+package br.com.fagnerabsynth.aplicativo.Models;
+
+/**
+ * Created by fagner on 30/10/2015.
+ */
+public class ProdutosMOD {
+    public int id = 0;
+    public String nome;
+    public String categoria;
+    public String descricao;
+    public int ativo;
+    public double valor;
+}
