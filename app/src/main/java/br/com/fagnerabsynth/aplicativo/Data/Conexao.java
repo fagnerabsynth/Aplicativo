806c5ae2f8986e1ebf2e1ca65187ad8b91e24ef9
fagnerabsynth <tagnisp@hotmail.com>
2015-10-31 17:57:00 -0200

    Crud do Banco de dados e tabela categoria para Spinner

--- app/src/main/java/br/com/fagnerabsynth/aplicativo/Data/Conexao.java
+++ app/src/main/java/br/com/fagnerabsynth/aplicativo/Data/Conexao.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 import br.com.fagnerabsynth.aplicativo.Models.ProdutosMOD;
 
@@ -18,13 +19,20 @@ import br.com.fagnerabsynth.aplicativo.Models.ProdutosMOD;
 public class Conexao extends SQLiteOpenHelper {
     public static final String TABELA = "usuarios";
     public static final String TABELA2 = "produtos";
+    public static final String TABELA3 = "categoria";
 
     private static final String ARQUIVO = "usuarios.db";
+
     private static final int VERSAO = 1;
+
     private static final String CRIARTABELA1 = "create table "
             + TABELA + "( id integer primary key autoincrement, email text not null, senha text not null);";
+
     private static final String CRIARTABELA2 = "create table "
             + TABELA2 + "( id integer primary key autoincrement, nome text not null, categoria text not null,descricao text not null, ativo flag INTEGER DEFAULT 0, valor DECIMAL(10,2) not null );";
+
+    private static final String CRIARTABELA3 = "create table "
+            + TABELA3 + "( id integer primary key autoincrement, categoria text not null unique);";
 
 
     private SQLiteDatabase db;
@@ -34,23 +42,18 @@ public class Conexao extends SQLiteOpenHelper {
     }
 
     private String md5(String s) {
-
         try {
-
             MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
             digest.update(s.getBytes());
             byte messageDigest[] = digest.digest();
-
             StringBuffer hexString = new StringBuffer();
             for (int i = 0; i < messageDigest.length; i++)
                 hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
             return hexString.toString();
-
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return "";
-
     }
 
 
@@ -59,6 +62,7 @@ public class Conexao extends SQLiteOpenHelper {
 
         db.execSQL(CRIARTABELA1);
         db.execSQL(CRIARTABELA2);
+        db.execSQL(CRIARTABELA3);
 
 
         Cursor rs = db.rawQuery("SELECT * FROM " + TABELA, null);
@@ -67,6 +71,23 @@ public class Conexao extends SQLiteOpenHelper {
             String[] dadosAdmin = {"admin@admin.com", md5("12345")};
             db.execSQL("Insert into " + TABELA + "(email,senha) values (?,?)", dadosAdmin);
         }
+
+        String temp = "";
+        Cursor tb2 = db.rawQuery("SELECT * FROM " + TABELA2, null);
+        if (tb2.getCount() > 0) {
+
+            if (tb2.moveToFirst()) {
+                do {
+                    temp = tb2.getString(tb2.getColumnIndex("categoria"));
+                    Cursor tb3 = db.rawQuery("SELECT * FROM " + TABELA3 + " where categoria = ?", new String[]{temp});
+                    if (tb3.getCount() == 0) {
+                        db.execSQL("DELETE  FROM " + TABELA3 + " where categoria = ?", new String[]{temp});
+                    }
+                } while (tb2.moveToNext());
+            }
+        }
+
+
     }
 
 
@@ -83,10 +104,32 @@ public class Conexao extends SQLiteOpenHelper {
         return true;
     }
 
+    public boolean adicionaCategoria(String cat) {
+        db = this.getWritableDatabase();
+        try {
+            db.execSQL("Insert into " + TABELA3 + " (categoria) values (?)", new String[]{cat});
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public ArrayList<String> selecionaCategorias() {
+        ArrayList<String> retorno = new ArrayList<String>();
+        db = this.getWritableDatabase();
+        Cursor rs = db.rawQuery("SELECT * FROM " + TABELA3 + " order by categoria asc", null);
+        if (rs.getCount() > 0) {
+            if (rs.moveToFirst()) {
+                do {
+                    retorno.add(rs.getString(rs.getColumnIndex("categoria")));
+                } while (rs.moveToNext());
+            }
+        }
+        return retorno;
+    }
 
     public boolean logar(String email, String senha) {
         db = this.getWritableDatabase();
-
         String[] dados = {email, md5(senha)};
         Cursor rs = db.rawQuery("SELECT * FROM " + TABELA + " where email = ? and senha = ?", dados);
         return rs.getCount() == 1;
--- app/src/main/java/br/com/fagnerabsynth/aplicativo/Views/Edicao.java
+++ app/src/main/java/br/com/fagnerabsynth/aplicativo/Views/Edicao.java
@@ -1,13 +1,119 @@
 package br.com.fagnerabsynth.aplicativo.Views;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fagnerabsynth.aplicativo.Data.Conexao;
 import br.com.fagnerabsynth.aplicativo.R;
 
 public class Edicao extends AppCompatActivity {
+
+    private Spinner spinner;
+    private String valorSpinner, add = "Adicionar nova categoria";
+    private LinearLayout linear, popup;
+    private RelativeLayout main;
+
+    public void cancelar(View v) {
+        linear.setVisibility(View.VISIBLE);
+        popup.setVisibility(View.INVISIBLE);
+        Toast.makeText(Edicao.this, "Cadastro de nova categoria\ncancelado pelo usuário", Toast.LENGTH_SHORT).show();
+        main.setBackgroundColor(Color.TRANSPARENT);
+
+        criaSpinner();
+
+    }
+
+
+    public void cadastraCategoria(View b) {
+
+        EditText input = (EditText) findViewById(R.id.novaCategoria);
+        String novaCategoria = input.getText().toString();
+        String mensagem = "";
+
+        if (TextUtils.isEmpty(novaCategoria)) {
+            mensagem = "Digite o nome da categoria";
+
+        } else {
+
+            try {
+
+                Conexao con = new Conexao(this);
+
+                if (con.adicionaCategoria(novaCategoria)) {
+                    mensagem = "Categoria adicionada com sucesso!";
+                    linear.setVisibility(View.VISIBLE);
+                    popup.setVisibility(View.INVISIBLE);
+                    main.setBackgroundColor(Color.TRANSPARENT);
+                    criaSpinner();
+                    input.setText("");
+                } else {
+                    mensagem = "Erro ao cadastrar categoria\n\nCategoria existente!";
+                }
+
+            } catch (Exception e) {
+                mensagem = "Não foi possivel adicionar categoria!";
+            }
+        }
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void criaSpinner() {
+        spinner.setPrompt("Seleciona uma categoria");
+
+        List<String> list = new ArrayList<String>();
+        list.add("");
+
+        list.add(add);
+
+        Conexao item = new Conexao(this);
+
+        ArrayList<String> dados = item.selecionaCategorias();
+        if (!dados.isEmpty()) {
+            for (String x : dados) {
+                list.add(x);
+            }
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+    }
+
+    public void executaSpinner() {
+
+        valorSpinner = spinner.getSelectedItem().toString();
+        if (!TextUtils.isEmpty(valorSpinner)) {
+            if (valorSpinner.equals(add)) {
+                linear.setVisibility(View.INVISIBLE);
+                popup.setVisibility(View.VISIBLE);
+
+                main.setBackgroundColor(Color.LTGRAY);
+                popup.setBackgroundColor(Color.WHITE);
+
+            }
+
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +121,25 @@ public class Edicao extends AppCompatActivity {
         setContentView(R.layout.edicao);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+        linear = (LinearLayout) findViewById(R.id.itens);
+        popup = (LinearLayout) findViewById(R.id.popup);
+
+        main = (RelativeLayout) findViewById(R.id.main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                executaSpinner();
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        criaSpinner();
         int id = getIntent().getIntExtra("id", 0);
 
         if (id == 0) {
